7b59096ad3808e47d0966a7532018512f1be25db
homi <87790194+homironi@users.noreply.github.com>
2025-05-11 21:14:09 +0900

    [add] 記事ページのお試し：FrontMatterの分離お試し (#222)

---
+++ src/app/articles/[id]/page.tsx
@@ -0,0 +1,24 @@
+import fs from "fs";
+import matter from "gray-matter";
+
+export async function generateStaticParams() {
+  return [
+    { id: "20250429021614" }];
+}
+
+export default function ArticlePage() {
+  const raw = fs.readFileSync("_contents/articles/20250429021614.md", "utf-8");
+  const { data, content } = matter(raw);
+  return (
+    <div>
+      <h2>frontMatter</h2>
+      <p>
+        { JSON.stringify(data) }
+      </p>
+      <h2>content</h2>
+      <p>
+        { content}
+      </p>
+    </div>
+  );
+}
