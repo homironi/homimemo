f33e268ed3529a039c5e8711bb9d2c8002431dfd
mimimiRoni <nakanemiu@gmail.com>
2025-05-12 20:05:44 +0900

    [update] idがなければ付与するように

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -9,6 +9,8 @@ export async function generateStaticParams() {
 export default function ArticlePage() {
   const raw = fs.readFileSync("_contents/articles/20250429021614.md", "utf-8");
   const { data, content } = matter(raw);
+  const frontMatter = { id: data.id ? data.id : "20250429021614", ...data };
+  fs.writeFileSync("_contents/articles/20250429021614.md", matter.stringify(content, frontMatter));
   return (
     <div>
       <h2>frontMatter</h2>
