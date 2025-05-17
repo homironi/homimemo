3f407abd1cdae4a3c679bad7d1cb64fb56c34b82
homi <87790194+homironi@users.noreply.github.com>
2025-05-17 21:57:14 +0900

    [add] 目次自動生成Componentを追加 (#246)
    ![image](https://github.com/user-attachments/assets/60ef038e-c4cd-4107-a6f1-393bcb5b3dc3)
    
    
    ## 関連
    
    -
    https://nextjs-ja-translation-docs.vercel.app/docs/advanced-features/dynamic-import

---
+++ src/components/TableOfContents/index.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect } from "react";
+import tocbot from "tocbot";
+import styles from "./index.module.css";
+
+type Props = {
+  tocContentSourceIdName: string;
+};
+
+/**
+ * 目次コンポーネント
+ * @param root0 引数オブジェクト
+ * @param root0.tocContentSourceIdName 目次を生成する対処についているコンテンツのID名
+ * @returns 目次のJSX要素
+ */
+export default function TableOfContents({ tocContentSourceIdName }: Props) {
+  useEffect(() => {
+    tocbot.init({
+      tocSelector: `.${styles.toc}`,
+      contentSelector: `#${tocContentSourceIdName}`,
+      headingSelector: "h2, h3",
+      activeLinkClass: styles["toc-link-active"],
+    });
+
+    return () => tocbot.destroy();
+  }, [tocContentSourceIdName]);
+
+  return (
+    <nav>
+      <h2>自動生成目次</h2>
+      <div className={ styles.toc } />
+    </nav>
+  );
+}
