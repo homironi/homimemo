eb63b8fb74f54cee0d18da8b8cf4221bcb6b8a20
mimimiRoni <nakanemiu@gmail.com>
2025-05-20 07:30:15 +0900

    [update] lint違反の修正

--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import { Button } from './Button';
-import './header.css';
+import { Button } from "./Button";
+import "./header.css";
 
 type User = {
   name: string;
@@ -37,19 +37,24 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps
         <h1>Acme</h1>
       </div>
       <div>
-        {user ? (
-          <>
-            <span className="welcome">
-              Welcome, <b>{user.name}</b>!
-            </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
-          </>
-        ) : (
-          <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
-          </>
-        )}
+        {user
+          ? (
+              <>
+                <span className="welcome">
+                  Welcome,
+                  {" "}
+                  <b>{user.name}</b>
+                  !
+                </span>
+                <Button size="small" onClick={ onLogout } label="Log out" />
+              </>
+            )
+          : (
+              <>
+                <Button size="small" onClick={ onLogin } label="Log in" />
+                <Button primary size="small" onClick={ onCreateAccount } label="Sign up" />
+              </>
+            )}
       </div>
     </div>
   </header>
