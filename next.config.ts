05b2d17d7c6ab5dcf6c804e1927601ce683edd90
mimimiRoni <nakanemiu@gmail.com>
2025-05-25 14:43:32 +0900

    [update] ファイル名/index.htmlになるように設定変更
    https://nextjs.org/docs/app/guides/static-exports

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "_public",
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
