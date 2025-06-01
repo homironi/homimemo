0acf5b9cf6be4085ac07344526f4b254a9a141ff
homi <87790194+mimimiRoni@users.noreply.github.com>
2025-06-01 21:09:04 +0900

    [update] Footerにロゴリンク追加 (#281)
    ![image](https://github.com/user-attachments/assets/a4516f20-0d27-4b83-b463-1954f4dd77e1)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./Footer.module.css";
+
 /**
  * フッター
  * @returns フッターコンポーネントのJSX要素
  */
 export function Footer() {
+  const nowYear = new Date().getFullYear();
   return (
     <footer>
-      <p>まだ仮のフッター</p>
+      {/* TODO: Footer 用 ナビゲーション */}
+      {/* TODO: 外部アカウントリンク */}
+      <div className={ styles.container }>
+        <div className={ styles["logo-container"] }>
+          <Link href="/">
+            <Image
+              src="/images/logo.svg"
+              alt="Logo"
+              fill
+              className={ styles.logo }
+            />
+          </Link>
+        </div>
+        <p
+          className={ styles["copy-light"] }
+          translate="no"
+        >
+          © 2023-
+          {nowYear.toString()}
+          {" "}
+          homironi
+        </p>
+      </div>
     </footer>
   );
 }
