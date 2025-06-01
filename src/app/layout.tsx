b11702a4adce261153595291d7c086796269620c
homi <87790194+homironi@users.noreply.github.com>
2025-06-01 19:06:53 +0900

    [add] HeaderとFooterを追加 (#278)
    - HeaderとFooterを追加
    - （ついでに）Articleの記事部分を`article`タグに変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
 import type { Metadata } from "next";
 import { Kosugi_Maru } from "next/font/google";
 import "./globalElement.css";
@@ -29,7 +31,9 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={ `${kosugiMaru.className} ${kosugiMaru.variable}` }>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+/**
+ * ヘッダーコンポーネント
+ * @returns ヘッダーコンポーネントのJSX要素
+ */
+export function Header() {
+  return (
+    <header>
+      <p>まだ仮のヘッダー</p>
+    </header>
+  );
+}
