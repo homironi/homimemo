0598724768acab0f500f9bee6a60ab697defb75c
homi <87790194+homironi@users.noreply.github.com>
2025-06-07 20:50:31 +0900

    [update] Componentにクラス名を渡せるように変更 (#288)

---
+++ src/components/TableOfContents/TableOfContents.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useEffect } from "react";
+import tocbot from "tocbot";
+import styles from "./index.module.css";
+
+export type TableOfContentsProps = {
+  className?: string;
+  tocContentSourceIdName: string;
+};
+
+/**
+ * 目次コンポーネント
+ * @param root0 引数オブジェクト
+ * @param root0.tocContentSourceIdName 目次を生成する対処についているコンテンツのID名
+ * @param root0.className 付与したいクラス名
+ * @returns 目次のJSX要素
+ */
+export function TableOfContents({
+  className,
+  tocContentSourceIdName,
+}: TableOfContentsProps) {
+  useEffect(() => {
+    tocbot.init({
+      tocSelector: `.${styles.toc}`,
+      contentSelector: `#${tocContentSourceIdName}`,
+      headingSelector: "h2, h3",
+      activeLinkClass: styles["toc-link-active"],
+    });
+
+    return () => tocbot.destroy();
+  }, [tocContentSourceIdName]);
+
+  return (
+    <nav className={ className ?? "" }>
+      <h2>自動生成目次</h2>
+      <div className={ styles.toc } />
+    </nav>
+  );
+}
