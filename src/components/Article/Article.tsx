82fcc9cf31e784e0608014a62ed50bab77105a60
mimimiRoni <nakanemiu@gmail.com>
2025-06-07 17:44:36 +0900

    [update] 目次Componentも他のComponentと同じ構造に変更

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -12,7 +12,7 @@ import remarkGfm from "remark-gfm";
 import { safeParse } from "valibot";
 import "./prism.css"; // 記事内で使用するコードハイライトのPrismのスタイルを適用するためにインポート
 
-const DynamicToc = dynamic(() => import("@/components/TableOfContents").then(mod => mod.default));
+const DynamicToc = dynamic(() => import("@/components/TableOfContents").then(mod => mod.TableOfContents));
 const DynamicCodeCopyHandler = dynamic(() => import("@/components/CopyCodeHandler").then(mod => mod.default));
 const tocContentSourceIdName = "toc-source-content";
 
