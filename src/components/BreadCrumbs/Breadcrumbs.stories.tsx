d76d586465bbc4c1ff338e6876b5d2a1b48b28a2
mimimiRoni <nakanemiu@gmail.com>
2025-06-08 14:39:14 +0900

    [update] Storybookの更新

--- src/components/BreadCrumbs/Breadcrumbs.stories.tsx
+++ src/components/BreadCrumbs/Breadcrumbs.stories.tsx
@@ -9,5 +9,24 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Main: Story = {
+export const HomeOnly: Story = {
+  args: {
+    breadcrumbs: [
+    ],
+  },
+};
+
+export const Sample: Story = {
+  args: {
+    breadcrumbs: [
+      {
+        name: "一覧ページ",
+        href: "/hoge/list",
+      },
+      {
+        name: "各ページ",
+        href: "/hoge/hoge-page",
+      },
+    ],
+  },
 };
