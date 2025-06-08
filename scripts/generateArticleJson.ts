600d30e495be9828e968a118434231ca8556dcda
homi <87790194+homironi@users.noreply.github.com>
2025-06-08 17:32:31 +0900

    [update] 記事IDの生成取得の流れを変更 (#296)
    事前に生成してからビルドするように変更
    
    `npm run dev`のときはchokidarでファイルの変更を監視してjsonを更新するように

---
+++ scripts/generateArticleJson.ts
@@ -0,0 +1,62 @@
+/* eslint-disable no-console */
+// tsx で使用するスクリプトなのでログ出力のために警告を無効化する
+
+import { ArticleIdToPathMapElement, articlesDirectory, getIdToPathMap, idToPathMapFile } from "@/lib/article";
+import { ArticleMetaSchema } from "@/schemas/articleMeta";
+import fs from "fs";
+import matter from "gray-matter";
+import path from "path";
+import { safeParse } from "valibot";
+
+console.log("generating article id to path json...");
+
+generateIdToPathMap();
+console.log(getIdToPathMap());
+
+console.log("end generate article id to path json");
+
+/**
+ * 記事IDとそれに対応するマークダウンファイルのパスのMapを生成して保存する
+ */
+export function generateIdToPathMap() {
+  saveIdToPathMap(createIdToPathMap());
+}
+
+/**
+ * 記事のIDとファイルパスのマップ
+ * @returns 記事IDとファイルパスのマップの配列
+ */
+function createIdToPathMap(): ArticleIdToPathMapElement[] {
+  return fs.readdirSync(articlesDirectory, "utf-8")
+    .filter(file => file.endsWith(".md"))
+    .map((file) => {
+      const filePath = path.join(articlesDirectory, file); ;
+      const raw = fs.readFileSync(filePath, "utf-8");
+      const { data } = matter(raw);
+      const safeParsed = safeParse(ArticleMetaSchema, data);
+      return { file, safeParsed };
+    })
+    .filter(({ safeParsed }) => safeParsed.success) // TODO: draft も除外するようにする
+    .map<ArticleIdToPathMapElement>(({ file, safeParsed }) => {
+      // すでに filter で成功したものだけを対象にしているので、パースに失敗することはありませんが
+      // success をチェックしないと型補完が効かないので明示的にチェック
+      if (!safeParsed.success) {
+        throw new Error(`記事のメタデータのパースに失敗しました: ${file}`);
+      }
+
+      return {
+        id: safeParsed.output.id,
+        filePath: path.join(articlesDirectory, file),
+      };
+    });
+};
+
+/**
+ * 受け取ったデータを保存する
+ * @param idToPathMap 記事IDにとそのファイルパスのデータMap
+ * @description 後でページで記事ID→ファイルパスを参照するためのマップを保存
+ */
+function saveIdToPathMap(idToPathMap: ArticleIdToPathMapElement[]) {
+  fs.mkdirSync(path.dirname(idToPathMapFile), { recursive: true });
+  fs.writeFileSync(idToPathMapFile, JSON.stringify(idToPathMap, null, 2), "utf-8");
+}
