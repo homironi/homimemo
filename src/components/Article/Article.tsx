9648c0fb95ee6fe4a5952b5557619958808b0749
homi <87790194+homironi@users.noreply.github.com>
2025-06-08 13:15:33 +0900

    [update] タイトルを表示するように変更 (#293)

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,5 +1,6 @@
 import { rehypeCodeLangLabel, rehypeCodeToolContainer, rehypeCopyButton } from "@/lib/rehypePlugins/code";
 import { rehypeGfmTaskList } from "@/lib/rehypePlugins/gfmTaskList";
+import { ArticleMeta, ArticleMetaSchema } from "@/schemas/articleMeta";
 import fs from "fs";
 import matter from "gray-matter";
 import { MDXRemote } from "next-mdx-remote/rsc";
@@ -8,6 +9,7 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypePrism from "rehype-prism-plus";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
+import { parse } from "valibot";
 import styles from "./Article.module.css";
 import "./prism.css"; // 記事内で使用するコードハイライトのPrismのスタイルを適用するためにインポート
 
@@ -29,7 +31,8 @@ export function Article({ filePath }: ArticleProps) {
   const raw = fs.readFileSync(filePath, "utf-8");
 
   // TODO: FrontMatterをもとにmeta設定やタイトルなどを設定する
-  const { content } = matter(raw);
+  const { data, content } = matter(raw);
+  const validatedMeta = parse(ArticleMetaSchema, data);
 
   return (
     <div className={ styles.container }>
@@ -43,11 +46,19 @@ export function Article({ filePath }: ArticleProps) {
         className={ styles.article }
         content={ content }
         tocContentSourceIdName={ tocContentSourceIdName }
+        meta={ validatedMeta }
       />
       <div className={ styles["last-side"] }>TODO:ここにその他情報をのせる</div>
     </div>
   );
 }
+
+type ArticleMdxProps = {
+  className?: string;
+  content: string;
+  tocContentSourceIdName: string;
+  meta: ArticleMeta;
+};
 
 /**
  * 記事ページコンポーネント
@@ -55,22 +66,21 @@ export function Article({ filePath }: ArticleProps) {
  * @param root0.content 記事のMDXコンテンツ
  * @param root0.tocContentSourceIdName 目次のコンテンツソースとして扱う目印のID名
  * @param root0.className クラス名
+ * @param root0.meta 記事Meta情報
  * @returns 記事ページのコンポーネント
  */
 function ArticleMdx({
   className,
   content,
   tocContentSourceIdName,
-}: {
-  className?: string;
-  content: string;
-  tocContentSourceIdName: string;
-}) {
+  meta,
+}: ArticleMdxProps) {
   return (
     <article
       id={ tocContentSourceIdName }
       className={ className ?? "" }
     >
+      <h1>{ meta.title }</h1>
       <MDXRemote
         source={ content }
         options={ {
