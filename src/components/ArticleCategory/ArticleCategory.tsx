cf5c563a6c8f3e133f3984d300b577f6c9335bbc
mimimiRoni <nakanemiu@gmail.com>
2025-06-10 20:19:30 +0900

    [add] 記事カテゴリの記事内での表示用Component追加

---
+++ src/components/ArticleCategory/ArticleCategory.tsx
@@ -0,0 +1,21 @@
+import { CategoryMeta } from "@/schemas/article/category";
+import Link from "next/link";
+
+export type ArticleCategoryProps = {
+  meta: CategoryMeta;
+};
+
+/**
+ * 記事のカテゴリ表示
+ * @param root0 引数オブジェクト
+ * @param root0.meta カテゴリのMeta情報
+ * @returns 記事カテゴリ要素
+ * @todo スタイルは後から設定
+ */
+export function ArticleCategory({ meta }: ArticleCategoryProps) {
+  return (
+    <div>
+      <Link href={ `/categories/${meta.slug}` }>{ meta.name}</Link>
+    </div>
+  );
+}
