0f61c9114ea05c724b554fb951ad48792bfdc4f1
mimimiRoni <nakanemiu@gmail.com>
2025-06-15 19:28:39 +0900

    [update] StorybookでもSVGRを出してもエラーが出ないように変更

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs-vite";
+import svgr from "vite-plugin-svgr";
 
 const config: StorybookConfig = {
   stories: [
@@ -9,7 +10,7 @@ const config: StorybookConfig = {
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
-    "@storybook/addon-docs"
+    "@storybook/addon-docs",
   ],
   framework: {
     name: "@storybook/nextjs-vite",
@@ -18,6 +19,34 @@ const config: StorybookConfig = {
   staticDirs: [
     "../public",
   ],
+  viteFinal(config) {
+    config.plugins?.push(
+      svgr({
+        include: /\.svg$/,
+      }),
+    );
+    config.plugins = config.plugins?.flat().map((plugin) => {
+      if (
+        typeof plugin === "object"
+        && plugin !== null
+        && "name" in plugin
+        && plugin.name === "vite-plugin-storybook-nextjs-image"
+      ) {
+        return {
+          ...plugin,
+          resolveId(id, importer) {
+            if (id.endsWith(".svg")) {
+              return null;
+            }
+            // @ts-expect-error `resolveId` hook of vite-plugin-storybook-nextjs-image is a function
+            return plugin.resolveId(id, importer);
+          },
+        };
+      }
+      return plugin;
+    });
+    return config;
+  },
 };
 
 export default config;
