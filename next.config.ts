6bb579d496f26ab5b04de7d6fd9ce8a13ce0f10e
homi <87790194+homironi@users.noreply.github.com>
2025-06-15 19:56:42 +0900

    [update] SVGをimportして扱えるように設定 (#306)
    Storybook側だけ色の変更を反映するためのfillの削除などが効かないので、今は一旦アイコンの種類だけわかれば、色はNext側で反映できていればOKとしています。
    
    
    ![image](https://github.com/user-attachments/assets/2a857cf0-42cd-4583-bd3a-071746a04bbc)
    
    https://www.npmjs.com/package/@svgr/webpack
    
    - Next：
      - https://react-svgr.com/docs/next/
      - https://techshed.runbookdocs.com/docs/tips/2/optimize-svg
    - Storybook：https://zenn.dev/airrnot1106/articles/1ff694aedc953b
    
    ## アイコンのimportとCSSでの色設定コード例
    
    ```ts
    import Logo from "@/../public/images/icon_chara.svg";
    
    <Logo className={ styles.logo } />
    ```
    
    ```css
    .logo {
        fill: rgba(132 0 255);
    }
    ```

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,24 @@
 import type { NextConfig } from "next";
+import svgoConfig from "./svgo.config";
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      // src/app/icon.svg を @svgr/webpack の対象から除外する
+      // これはNext.jsのメタデータローダーに処理させるため
+      exclude: /src[\\/]app[\\/]icon\.svg$/, // Windows/Linux両対応のパス
+      loader: "@svgr/webpack",
+      options: {
+        svgoConfig,
+      },
+    });
+
+    return config;
+  },
+  images: {
+    disableStaticImages: true,
+  },
   output: "export",
   distDir: "_public",
   trailingSlash: true,
