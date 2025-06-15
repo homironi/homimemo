2fb825efd3a521bd2b7115e82b62f66ccb638777
mimimiRoni <nakanemiu@gmail.com>
2025-06-15 18:19:41 +0900

    [update] SVGをimportして扱えるように設定

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      // src/app/icon.svg を @svgr/webpack の対象から除外する
+      // これはNext.jsのメタデータローダーに処理させるため
+      exclude: /src[\\/]app[\\/]icon\.svg$/, // Windows/Linux両対応のパス
+      use: [
+        {
+          loader: "@svgr/webpack",
+        },
+      ],
+    },
+    );
+
+    return config;
+  },
+  images: {
+    disableStaticImages: true,
+  },
   output: "export",
   distDir: "_public",
   trailingSlash: true,
