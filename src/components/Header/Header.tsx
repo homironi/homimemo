24504ad5ae206e5bcd85c209eb298cc1cc9306e8
homi <87790194+mimimiRoni@users.noreply.github.com>
2025-06-21 21:06:43 +0900

    [update] ヘッダーのロゴサイズとリンクを調整 (#326)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,13 +10,14 @@ export function Header() {
   return (
     <header className={ styles.container }>
       <div className={ styles["logo-container"] }>
-        <Link href="/">
+        <Link href="/" className={ styles.link }>
           <Image
             src="/images/logo.svg"
             alt="Logo"
             width={ 256 }
             height={ 64 }
             priority
+            className={ styles.logo }
           />
         </Link>
       </div>
