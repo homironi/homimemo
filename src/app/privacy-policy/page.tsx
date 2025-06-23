f95b8aa00ae743ac46368393112864646150360f
mimimiRoni <nakanemiu@gmail.com>
2025-06-23 19:31:28 +0900

    [add] その他のページを仮で追加

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,12 @@
+/**
+ * プライバシーポリシーページのコンポーネント
+ * @returns プライバシーポリシーの内容を表示するページ
+ */
+export default function PrivacyPolicyPage() {
+  return (
+    <>
+      <h1>プライバシーポリシー</h1>
+      {/* TODO: 後で中身を作る */}
+    </>
+  );
+}
