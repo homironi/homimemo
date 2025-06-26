bd0e1853c27b0f1f058d90c6eeab74a3cfae889c
mimimiRoni <nakanemiu@gmail.com>
2025-06-26 11:47:08 +0900

    [update] 全記事一覧ページをComponentに置き換え

--- src/app/articles/page/[number]/page.tsx
+++ src/app/articles/page/[number]/page.tsx
@@ -1,10 +1,9 @@
-import { ArticleCard } from "@/components/ArticleCard";
+import { ArticleListPageLayout } from "@/components/ArticleListPageLayout";
+import { getPageLength } from "@/lib/article/listPage";
 import { getAllArticlesMeta } from "@/lib/server/article";
 import type { Metadata } from "next";
-import styles from "./page.module.css";
 
-// 記事一覧の1ページあたりの表示件数
-const perPage = 12;
+const title = "記事一覧";
 const listPagePathBase = "/articles/page/";
 
 type Params = {
@@ -28,7 +27,7 @@ export async function generateStaticParams(): Promise<Params[]> {
  */
 export async function generateMetadata(): Promise<Metadata> {
   return {
-    title: "記事一覧",
+    title: title,
     description: "すべての記事の一覧ページです。",
   };
 }
@@ -41,42 +40,15 @@ export async function generateMetadata(): Promise<Metadata> {
  */
 export default async function ArticlesPage({ params }: { params: Promise<Params> }) {
   const number = parseInt((await params).number);
-  const startNum = perPage * (number - 1);
-  const endNum = startNum + perPage;
   const articles = getAllArticlesMeta()
     .sort((a, b) => b.lastModDate.getTime() - a.lastModDate.getTime());
-  const pageArticles = articles.slice(startNum, endNum);
-  const pageNumbers = getPageLength(articles.length);
 
   return (
-    <div className={ styles.container }>
-      <h1>記事一覧</h1>
-      <p>{`全 ${articles.length} 件（${startNum + 1} 件目 ～ ${Math.min(endNum, articles.length)} 件目）`}</p>
-      <ol className={ styles.list }>
-        {pageArticles.map(article => (
-          <li key={ article.id } className={ styles.item }>
-            <ArticleCard meta={ article } />
-          </li>
-        ))}
-      </ol>
-      <ol>
-        { pageNumbers.map(pageNumber => (
-          <li key={ pageNumber }>
-            <a href={ `${listPagePathBase}${pageNumber}/` }>{pageNumber == number ? `${pageNumber}：今ここ` : pageNumber}</a>
-          </li>
-        ))}
-      </ol>
-    </div>
+    <ArticleListPageLayout
+      title={ title }
+      articles={ articles }
+      listPagePathBase={ listPagePathBase }
+      currentPageNumber={ number }
+    />
   );
-}
-
-/**
- * 記事の総数からページ番号の配列を生成する関数
- * @param length 記事の総数
- * @returns ページ番号の配列
- */
-function getPageLength(length: number): number[] {
-  const validLength = length <= 0 ? 1 : length;
-  const pageLength = Math.ceil(validLength / perPage);
-  return Array.from({ length: Math.ceil(pageLength) }, (_, i) => i + 1);
 }
