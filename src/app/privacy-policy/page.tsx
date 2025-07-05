6ef89175b51fc9846cb064b3736d3917d96b9b90
homi <87790194+homironi@users.noreply.github.com>
2025-07-05 16:49:29 +0900

    [update] プライバシーポリシーのページもマークダウンから構成するように更新 (#359)

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,12 +1,40 @@
+import { Article } from "@/components/server/Article";
+import { StaticArticleMeta, StaticArticleMetaSchema } from "@/schemas/article/meta";
+import fs from "fs";
+import matter from "gray-matter";
+import { Metadata } from "next";
+import path from "path";
+import { parse } from "valibot";
+
+const filePath = path.join("_contents", "static-articles", "privacy-policy.md");
+
+/**
+ * プライバシーポリシーページのメタデータを生成する関数
+ * @returns プライバシーポリシーページのメタデータ
+ */
+export async function generateMetadata(): Promise<Metadata> {
+  const { meta } = readArticleContent(filePath);
+
+  return {
+    title: meta.title,
+    description: meta.description,
+  };
+}
+
 /**
  * プライバシーポリシーページのコンポーネント
  * @returns プライバシーポリシーの内容を表示するページ
  */
 export default function PrivacyPolicyPage() {
+  const { meta, content } = readArticleContent(filePath);
   return (
-    <>
-      <h1>プライバシーポリシー</h1>
-      {/* TODO: 後で中身を作る */}
-    </>
+    <Article meta={ meta } content={ content } />
   );
+}
+
+function readArticleContent(filePath: string): { meta: StaticArticleMeta; content: string } {
+  const raw = fs.readFileSync(filePath, "utf-8");
+  const { data, content } = matter(raw);
+  const meta = parse(StaticArticleMetaSchema, data);
+  return { meta, content };
 }
