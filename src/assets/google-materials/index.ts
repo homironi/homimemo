9cee19a4222eed52b1cb743bf2e64036bd5bbb15
mimimiRoni <nakanemiu@gmail.com>
2025-07-06 14:08:05 +0900

    [update] iconのフォルダ変更

--- src/assets/google-materials/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as PublishDateIcon } from "@/assets/google-materials/calendar_today_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg";
-export { default as CategoryIcon } from "@/assets/google-materials/category_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg";
-export { default as LastModeDateIcon } from "@/assets/google-materials/published_with_changes_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg";
