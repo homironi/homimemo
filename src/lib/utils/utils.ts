b54e9adfb2af5c3e5a070f58af208c65b9ecc18a
homironi <nakanemiu@gmail.com>
2025-08-22 16:03:03 +0900

    [update] og設定追加

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -1,5 +1,51 @@
+import { OpenGraph } from "next/dist/lib/metadata/types/opengraph-types";
+import { Twitter } from "next/dist/lib/metadata/types/twitter-types";
+import { defaultArticleThumbnail } from "../article";
+
 export const siteName = "ほみろに";
+export const defaultDescription =
+  "ゲームやお絵かきなどいろんなことを書く、ほみの個人サイトです。";
+
+export function createDefaultOG(
+  title?: string,
+  description?: string,
+  slug?: string,
+  thumbnail?: string
+): OpenGraph {
+  const url = createUrlFromSlug(slug || "/");
+  const titleText = title ? createTitleFromTemplate(title) : siteName;
+  return {
+    siteName: siteName,
+    title: titleText,
+    description: description || defaultDescription,
+    url,
+    images: [
+      {
+        url: thumbnail || defaultArticleThumbnail,
+        alt: titleText,
+      },
+    ],
+  };
+}
+
+export function createDefaultTwitter(
+  title?: string,
+  description?: string,
+  thumbnail?: string
+): Twitter {
+  return {
+    card: "summary_large_image",
+    site: siteName,
+    title: title ? createTitleFromTemplate(title) : siteName,
+    description: description || defaultDescription,
+    images: [thumbnail || defaultArticleThumbnail],
+  };
+}
 
 export function createTitleFromTemplate(title: string) {
   return `${title} | ${siteName}`;
+}
+
+export function createUrlFromSlug(slug: string) {
+  return `https://homironi.com${slug}`;
 }
