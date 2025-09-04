2ed9439cabb2a826c357079e9c02f9714b840b46
homi <87790194+homironi@users.noreply.github.com>
2025-09-04 21:31:32 +0900

    [update] トップページに更新履歴を表示するように (#504)
    <img width="1393" height="550" alt="image"
    src="https://github.com/user-attachments/assets/5cdf5d85-db49-43d0-aad3-c8804d8e742b"
    />

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { ArticleLink } from "@/components/ArticleLink";
-import { articlesListPagePath } from "@/lib/article";
+import { articlesListPagePath, createArticleDetailPath } from "@/lib/article";
 import { formatDate } from "@/lib/date";
 import { getAllArticlesMeta } from "@/lib/server/article";
 import { createDefaultOG } from "@/lib/utils";
 import { Metadata } from "next";
 import Link from "next/link";
+import { JSX } from "react";
 import styles from "./page.module.css";
 
 const newArticlesCount = 10;
@@ -34,6 +35,7 @@ export default function Home() {
     .slice(0, newArticlesCount);
   return (
     <div className={styles.container}>
+      <Changelog viewNum={4} />
       <h2 className={styles.title}>新着記事</h2>
       <ol className={styles.list}>
         {latestArticles.map((article) => {
@@ -56,3 +58,96 @@ export default function Home() {
     </div>
   );
 }
+
+type ChangelogProps = {
+  viewNum: number;
+};
+
+type ChangelogData = {
+  id: string;
+  date: string;
+  description: JSX.Element;
+};
+
+function Changelog({ viewNum }: ChangelogProps) {
+  const allArticles: ChangelogData[] = getAllArticlesMeta()
+    .map((article) => {
+      const publishedDate = formatDate(article.publishDate, "YYYY-MM-DD");
+      const modifiedDate = formatDate(article.lastModDate, "YYYY-MM-DD");
+      // 公開日と更新日が同じ場合は更新履歴を出さない
+      const isModified = publishedDate !== modifiedDate;
+
+      let data: ChangelogData[] = [
+        {
+          id: `${article.id}-published`,
+          date: publishedDate,
+          description: (
+            <>
+              【記事公開】
+              <a href={createArticleDetailPath(article.id)}>{article.title}</a>
+            </>
+          ),
+        },
+      ];
+
+      if (isModified) {
+        data.push({
+          id: `${article.id}-modified`,
+          date: modifiedDate,
+          description: (
+            <>
+              【記事更新】
+              <a href={createArticleDetailPath(article.id)}>{article.title}</a>
+            </>
+          ),
+        });
+      }
+
+      return data;
+    })
+    .flat();
+
+  const combinedChangelog: ChangelogData[] = [
+    ...changelogData.map((item) => ({ ...item, id: crypto.randomUUID() })),
+    ...allArticles,
+  ]
+    .sort((a, b) => (a.date < b.date ? 1 : -1))
+    .slice(0, viewNum);
+
+  return (
+    <div>
+      <h2 className={styles.title}>変更履歴</h2>
+      <ol className={styles.list}>
+        {combinedChangelog.map((log) => (
+          <li key={log.id} className={styles.item}>
+            <time dateTime={log.date}>{log.date.replace(/-/g, "/")}</time>：
+            {log.description}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+const changelogData: Omit<ChangelogData, "id">[] = [
+  {
+    date: "2025-09-03",
+    description: <>サイトのホスティングをCloudflare Workersに変更しました</>,
+  },
+  {
+    date: "2025-07-13",
+    description: <>サイトをNext.jsを中心としたSSGにリニューアルしました</>,
+  },
+  {
+    date: "2023-12-04",
+    description: (
+      <>
+        ダークテーマ対応してみました。OSのテーマ設定に合わせて自動で切り替わります
+      </>
+    ),
+  },
+  {
+    date: "2023-08-23",
+    description: <a href="/articles/qnhckrrtrx8xv662s0ox840y/">サイト公開</a>,
+  },
+];
