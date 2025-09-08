48f742047be529b343053155c28f08ec63366b96
homi <87790194+homironi@users.noreply.github.com>
2025-09-08 21:03:29 +0900

    [update] TextBlockはh要素ではなくp要素に変更 (#513)

--- src/components/TextBlock/TextBlock.stories.tsx
+++ src/components/TextBlock/TextBlock.stories.tsx
@@ -8,24 +8,6 @@ const meta = {
   parameters: {
     layout: "padded",
   },
-  argTypes: {
-    blockType: {
-      control: { type: "select" },
-      options: [
-        "info",
-        "warning",
-        "error",
-        "success",
-        "note",
-        "tip",
-        "question",
-      ],
-    },
-    titleLevel: {
-      control: { type: "select" },
-      options: ["h1", "h2", "h3", "h4", "h5", "h6"],
-    },
-  },
 } satisfies Meta<typeof TextBlock>;
 
 export default meta;
