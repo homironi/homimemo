896048b6b601307af1167b9dce1d8433503c0cb9
homi <87790194+homironi@users.noreply.github.com>
2025-09-25 14:27:03 +0900

    [add] 対話形式で新規記事ファイルの作成ができるスクリプト追加 (#581)
    <img width="433" height="149" alt="image"
    src="https://github.com/user-attachments/assets/af8684de-f9c0-4a30-bdd5-6d6e0b3a48f5"
    />
    
    <img width="360" height="98" alt="image"
    src="https://github.com/user-attachments/assets/0310ee50-e6eb-4b4b-98fb-cac3b4d59903"
    />
    
    
    ローカルで作成したい時はこれを使うとよき
    
    カテゴリは選択式

--- scripts/article/generateCategoriesJson.ts
+++ scripts/article/generateCategoriesJson.ts
@@ -1,26 +1,10 @@
 import { categoriesMetaFilePath } from "@/lib/server/article";
-import { CategoryMetaSchema } from "@/schemas/article/meta";
 import fs from "fs";
-import matter from "gray-matter";
-import path from "path";
-import { parse } from "valibot";
-
-const sourceDirectoryName = path.join("_contents", "categories");
+import { generateCategoriesJson } from "../lib/article";
 
 console.log("generating categories json...");
 
-fs.mkdirSync(path.dirname(categoriesMetaFilePath), { recursive: true });
-
-const data = fs.readdirSync(sourceDirectoryName, "utf-8")
-  .filter(file => file.endsWith(".md"))
-  .map((file) => {
-    const filePath = path.join(sourceDirectoryName, file); ;
-    const raw = fs.readFileSync(filePath, "utf-8");
-    const { data } = matter(raw);
-    return parse(CategoryMetaSchema, data);
-  });
-
-fs.writeFileSync(categoriesMetaFilePath, JSON.stringify(data, null, 2), "utf-8");
+generateCategoriesJson();
 
 console.log({ categories: JSON.parse(fs.readFileSync(categoriesMetaFilePath, "utf-8")) });
 console.log("end generate categories json");
