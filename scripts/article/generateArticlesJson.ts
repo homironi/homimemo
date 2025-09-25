02b9383702a940a0268e903cb2e56443f30bdc3f
homironi <nakanemiu@gmail.com>
2025-09-25 14:02:03 +0900

    [update] 主にpackageで使えるscriptsとして使用しているscriptをリファクタ

--- scripts/article/generateArticlesJson.ts
+++ scripts/article/generateArticlesJson.ts
@@ -4,30 +4,24 @@ import fs from "fs";
 import matter from "gray-matter";
 import path from "path";
 import { parse } from "valibot";
-
-const sourceDirectoryName = path.join("_contents", "articles");
+import { articleDirectoryName } from "../lib/article";
 
 console.log("generating articles json...");
-generateArticlesJson();
+
+fs.mkdirSync(path.dirname(articlesMetaFilePath), { recursive: true });
+const data = fs.readdirSync(articleDirectoryName, "utf-8")
+  .filter(file => file.endsWith(".md"))
+  .map((file) => {
+    const filePath = path.join(articleDirectoryName, file); ;
+    const raw = fs.readFileSync(filePath, "utf-8");
+    const { data } = matter(raw);
+    return convertMetaFromRaw(parse(ArticleRawMetaSchema, data));
+  })
+  .filter((meta): meta is ArticleMeta => {
+    return meta.id !== undefined && !meta.draft;
+  });
+
+console.log({ articles: data });
+fs.writeFileSync(articlesMetaFilePath, JSON.stringify(data, null, 2), "utf-8");
+
 console.log("end generate articles json");
-
-function generateArticlesJson() {
-  fs.mkdirSync(path.dirname(articlesMetaFilePath), { recursive: true });
-  const data = createArticlesData();
-  console.log({ articles: data });
-  fs.writeFileSync(articlesMetaFilePath, JSON.stringify(data, null, 2), "utf-8");
-}
-
-function createArticlesData(): ArticleMeta[] {
-  return fs.readdirSync(sourceDirectoryName, "utf-8")
-    .filter(file => file.endsWith(".md"))
-    .map((file) => {
-      const filePath = path.join(sourceDirectoryName, file); ;
-      const raw = fs.readFileSync(filePath, "utf-8");
-      const { data } = matter(raw);
-      return convertMetaFromRaw(parse(ArticleRawMetaSchema, data));
-    })
-    .filter((meta): meta is ArticleMeta => {
-      return meta.id !== undefined && !meta.draft;
-    });
-}
--- scripts/article/generateTagsJson.ts
+++ scripts/article/generateTagsJson.ts
@@ -1,5 +1,5 @@
 import { tagsMetaFilePath } from "@/lib/server/article";
-import { TagMeta, TagMetaSchema } from "@/schemas/article/meta";
+import { TagMetaSchema } from "@/schemas/article/meta";
 import fs from "fs";
 import matter from "gray-matter";
 import path from "path";
@@ -8,23 +8,19 @@ import { parse } from "valibot";
 const sourceDirectoryName = path.join("_contents", "tags");
 
 console.log("generating tags json...");
-generateTagsJson();
+
+const data = fs.readdirSync(sourceDirectoryName, "utf-8")
+  .filter(file => file.endsWith(".md"))
+  .map((file) => {
+    const filePath = path.join(sourceDirectoryName, file); ;
+    const raw = fs.readFileSync(filePath, "utf-8");
+    const { data } = matter(raw);
+    return parse(TagMetaSchema, data);
+  });
+
+console.log({ tags: data });
+
+fs.mkdirSync(path.dirname(tagsMetaFilePath), { recursive: true });
+fs.writeFileSync(tagsMetaFilePath, JSON.stringify(data, null, 2), "utf-8");
+
 console.log("end generate tags json");
-
-function generateTagsJson() {
-  fs.mkdirSync(path.dirname(tagsMetaFilePath), { recursive: true });
-  const data = generateTags();
-  console.log({ tags: data });
-  fs.writeFileSync(tagsMetaFilePath, JSON.stringify(data, null, 2), "utf-8");
-}
-
-function generateTags(): TagMeta[] {
-  return fs.readdirSync(sourceDirectoryName, "utf-8")
-    .filter(file => file.endsWith(".md"))
-    .map((file) => {
-      const filePath = path.join(sourceDirectoryName, file); ;
-      const raw = fs.readFileSync(filePath, "utf-8");
-      const { data } = matter(raw);
-      return parse(TagMetaSchema, data);
-    });
-}
