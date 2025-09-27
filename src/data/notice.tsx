059fbca5f2f27ab450c932657235677b1b7b0f85
homi <87790194+homironi@users.noreply.github.com>
2025-09-27 16:09:52 +0900

    [update] TOPの表示を更新 (#586)
    <img width="1914" height="953" alt="image"
    src="https://github.com/user-attachments/assets/74877ba8-9541-43ed-b500-4706053ae9ee"
    />
    
    <img width="318" height="588" alt="image"
    src="https://github.com/user-attachments/assets/675959c4-64a3-411b-a3f8-549b50389ecf"
    />

---
+++ src/data/notice.tsx
@@ -0,0 +1,36 @@
+import { createArticleDetailPath } from "@/lib/article";
+import Link from "next/link";
+import { JSX } from "react";
+
+type NoticeData = {
+  id: string;
+  date: string;
+  description: JSX.Element;
+};
+
+/**
+ * HOMEのお知らせデータ
+ */
+export const noticeData: Omit<NoticeData, "id">[] = [
+  {
+    date: "2025-09-03",
+    description: <>サイトをCloudflare Workersに移行しました</>,
+  },
+  {
+    date: "2025-07-13",
+    description: <>サイトをNext.jsを中心としたSSGにリニューアルしました</>,
+  },
+  {
+    date: "2023-12-04",
+    description: (
+      <>
+        <Link href={ createArticleDetailPath("fdjs9rs2bn7ugq9rf8ysa28h") }>ダークテーマ対応してみました。</Link>
+        OSのテーマ設定に合わせて自動で切り替わります
+      </>
+    ),
+  },
+  {
+    date: "2023-08-23",
+    description: <Link href={ createArticleDetailPath("qnhckrrtrx8xv662s0ox840y") }>サイト公開</Link>,
+  },
+];
