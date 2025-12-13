6a908cf80eb9042f7f7d80a2d1fede4ea6c74089
homironi <87790194+homironi@users.noreply.github.com>
2025-12-13 19:48:08 +0900

    [update] ホームページを追加し直し
    (cherry picked from commit 679d5577c777fe0b72d59df1d6a4ec8d929adb24)

--- src/lib/collection.ts
+++ src/lib/collection.ts
@@ -1,4 +1,5 @@
-import { getCollection } from "astro:content";
+import type { ArticleMeta } from "@/content.config";
+import { getCollection, getEntry } from "astro:content";
 
 /**
  * すべての記事を取得する
@@ -6,4 +7,18 @@ import { getCollection } from "astro:content";
  */
 export function getAllArticles(){
   return getCollection("articles");
+}
+
+/**
+ * 記事IDに一致するメタデータを取得する
+ * @param id 記事ID
+ * @returns 記事のメタデータ
+ */
+export async function getArticle(id: string): Promise<ArticleMeta> {
+  const entry = await getEntry("articles", id);
+  if (!entry) {
+    throw new Error(`Article with ID "${id}" not found.`);
+  }
+
+  return entry.data;
 }
