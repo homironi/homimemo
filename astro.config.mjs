69060ef4e8d32438187ebb0b9cc9a2bb56615f2f
homi <87790194+homironi@users.noreply.github.com>
2025-12-25 20:30:15 +0900

    [update] 設定調整 (#711)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,10 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    outDir: "./_public",
+  site: "https://homironi.com",
+  outDir: "./_public",
+  trailingSlash: "always",
+  build: {
+    format: "directory",
+  }
 });
