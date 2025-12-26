18f65e902323cf2099ff34b39c8782c7f0f06cef
homironi <87790194+homironi@users.noreply.github.com>
2025-12-26 10:01:12 +0900

    [delete] 念の為残していた_oldフォルダごと削除

--- _old/scripts/article/generateTagsJson.ts
+++
@@ -1,7 +0,0 @@
-import { generateTagsJson } from "../lib/article";
-
-console.log("generating tags json...");
-
-generateTagsJson();
-
-console.log("end generate tags json");
--- _old/src/assets/logos/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "@/assets/logos/GitHub/GitHubLogo";
-export * from "@/assets/logos/note/NoteLogo";
-export * from "@/assets/logos/x/XLogo";
-
