e819415e8580436b41090c854f5421786686bc35
homi <87790194+homironi@users.noreply.github.com>
2025-12-31 10:58:22 +0900

    [update] おすすめ記事の変更 (#826)

--- src/data/recommendedArticle.ts
+++ src/data/recommendedArticle.ts
@@ -6,7 +6,6 @@ import { type ArticleMeta } from "@/schemas/article/meta";
  * @see recommendedArticles おすすめ記事Meta
  */
 export const recommendedArticleIds : string[] = [
-  "u7ax9hdyzzu3zkp5grbjazmt",
   "4m0x8paz57s6xzdv6llba90r",
   "1rzo1xgl9sna9y2apleb8of4",
   "y9gwahvcxi4fkbq52ws3a5q6",
