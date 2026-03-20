cde9843dd34eb45a6312ef9a683e151d7c02c16f
homironi <87790194+homironi@users.noreply.github.com>
2026-03-20 21:03:10 +0900

    [update] 日時の文字列変換関数をリファクタ

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -11,33 +11,39 @@ export type SupportedDateFormat =
  * @description 必要なフォーマットが増えたら、switch文に追加する
  */
 export function formatDate(date: Date, format: SupportedDateFormat): string {
+  const YYYY = zeroPad(date.getFullYear(), 4);
+  const MM = zeroPad(date.getMonth() + 1, 2);
+  const DD = zeroPad(date.getDate(), 2);
+
   switch (format) {
     case "YYYY-MM-DD":
-      return date.toLocaleDateString("en-CA", {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-      });
+      return `${YYYY}-${MM}-${DD}`;
 
     case "YYYY/MM/DD":
-      return date.toLocaleDateString("ja-JP", {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-      });
+      return `${YYYY}/${MM}/${DD}`;
 
     case "YYYYMMDDHHmmss":{
-      const yearStr = date.getFullYear().toString().padStart(4, "0");
-      const monthStr = (date.getMonth()+1).toString().padStart(2, "0");
-      const dateStr = date.getDate().toString().padStart(2, "0");
-      const hourStr = date.getHours().toString().padStart(2, "0");
-      const minutesStr =date.getMinutes().toString().padStart(2, "0");
-      const secondsStr = date.getSeconds().toString().padStart(2, "0");
+      const HH = zeroPad(date.getHours(), 2);
+      const mm = zeroPad(date.getMinutes(), 2);
+      const ss = zeroPad(date.getSeconds(), 2);
 
-      return `${yearStr}${monthStr}${dateStr}${hourStr}${minutesStr}${secondsStr}`;
+      return `${YYYY}${MM}${DD}${HH}${mm}${ss}`;
     }
       
     default:
+      // SupportedDateFormatに新しいフォーマットを追加した場合、switch文にケースを追加し忘れたらコンパイルエラーになるようにして気付きやすくするためのコード
+      // eslint-disable-next-line no-case-declarations, @typescript-eslint/no-unused-vars
+      const _exhaustiveCheck: never = format;
       throw new Error(`Unsupported date format: ${format}`);
   }
+}
+
+/**
+ * 0埋めする関数
+ * @param num 0埋めしたい数値
+ * @param length 0埋め後の文字列の長さ
+ * @returns 0埋めされた文字列
+ */
+function zeroPad(num: number, length: number): string {
+  return num.toString().padStart(length, "0");
 }
